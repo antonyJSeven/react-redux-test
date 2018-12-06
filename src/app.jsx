cb156f70278a9aae0e7835e88889449bb7a66b4a
lexszentmiklosy <lex.szentmiklosy@thomsonreuters.com>
2018-12-06 10:24:14 -0500

    add eslint

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './hello';
 
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello name="Lex" />,
     document.getElementById('root')
