bff16138e19c8590cfe62f082e2b34dc3f64e806
antonyjseven <antonyjseven@gmail.com>
2018-12-12 20:13:25 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import DateComponent from './components/DateComponent/dateComponent'
 import './App.css';
 
+// import store from './store'
+
+// import '@elf/elf-theme-elemental/light';
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
   render() {
     return (
       <div className="App">
+          <DateComponent/>
+          <div>azazaz</div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- src/components/DateComponent/dateComponent.js
+++ src/components/DateComponent/dateComponent.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './style.css'
+
+import '@elf/polyfills';
+import '@elf/coral-button'; // Element
+import '@elf/coral-input'; // Element
+import '@elf/elf-theme-elemental/light/coral-button';
+// import '@elf/elf-theme-elemental/light/coral-input';
+
 export default class DateComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            time: new Date().toLocaleTimeString()
+        };
+        this.startTimer();
+    }
+
+    startTimer = () => {
+        console.log(this.props);
+        // console.log(this.props.store.getState());
+        setInterval( () => {
+            this.setState({time : new Date().toLocaleTimeString()});
+        }, 1000)
+    };
+
     render() {
         return (
             <div className="date">
-                <input type="text" placeholder={new Date().toString()}/>
-                {new Date().toString()}
+                <input type="text" placeholder={this.state.time}/>
+                <coral-input icon="bubble" placeholder={this.state.time}></coral-input>
+                <coral-button>try try try</coral-button>
             </div>
         );
     }
